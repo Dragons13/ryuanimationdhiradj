0543c340a36bbd3868e53cb47e9fbe18b306e36a
codegorilla <codegorilla@codegorillas-MacBook-Air-2.local>
2019-03-07 10:21:06 +0100

    heroku push test

--- index.php
+++ index.php
@@ -1,1 +1,101 @@
-<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+        <style>
+                body {
+                  color: white;
+                }
+
+        </style>
+
+    
+
+    
+<title>Street Fighter - RYU</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+</head>
+
+
+
+
+
+
+
+<body>
+    <header>
+
+        <div class="row">
+            <div class="logo">
+                <img src="logo.png">
+            </div>
+        
+
+
+        </div>
+
+        <div class="button">
+                <a href="" class="btn btn-one"> Watch The Site</a>
+                <a href="" class="btn btn-two"> More Information</a>
+
+            </div>
+
+            
+            
+              
+
+
+        
+
+
+
+
+            
+
+            
+
+
+
+        </div>
+
+        
+
+
+        <style>
+                div.a {
+                  text-align: center;
+                } 
+                </style>
+                </head>
+                <body>
+
+                    <div class="parent">
+                        <div class="ryu"></div>
+                      </div>
+                
+                
+                <div class="a">
+
+                        <h1 style="font-size:8vw;">StreetFighter - RYU:</h1>
+                        <p style="font-size:2vw;">In het eerste Street Fighter-spel draagt Ryu een gescheurde witte karate-gi met een witte hachimaki (hoofdband), rode handschoenen en rode schoenen. Ook een kenmerkende uiterlijkheid is de Fūrinkazan (风 林 火山, Kanji-teken dat staat voor Wind, Bos, Vuur en Berg), gevechtsstandaard van de historische Japanse militaire leider Shingen Takeda, geborduurd in zijn obi. In Street Fighter II is Ryu wat ouder gemaakt, met bruin haar en bruine handschoenen, en een rode hachimaki. Hij vecht deze keer ook op blote voeten. In de Alpha-serie lijkt Ryu meer op hoe hij afgebeeld is in de originele Street Fighter, met lichtrood haar en een witte hachimaki. In de Street Fighter III-spellen heeft Ryu zwart haar en stoppels op zijn gezicht om zijn groei in leeftijd te laten zien. Street Fighter IV is chronologisch ingesteld tussen II en III en heeft Ryu dus een meer volwassen uitstraling dan in Street Fighter II, maar ziet hij er nog niet zo oud uit als in Street Fighter III.
+                        
+                Omdat hij het enige speelbare personage in de originele Street Fighter was, wilde Ryu-ontwerper Manabu Takemura hem gemakkelijk herkenbaar maken. In Street Fighter II was het personage geselecteerd voor opname vanwege zijn aanwezigheid in de eerste game, als voorbeeld van een Japanse martial artist. Naarmate de serie vorderde, werd het ontwerp gespierder gemaakt om samen te laten vallen met het concept, terwijl zijn witte gi (door het ontwikkelteam beschouwd als zijn meest kenmerkende eigenschap) bedoeld was om kijkers te laten weten dat hij "een karatemeester op het eerste gezicht" was. Ryu heeft bloedgroep O, is 175 cm lang en weegt 68 kg.
+                        
+                        
+                Ryu is een stille, en serieuze persoon. Zijn houding is het tegengestelde als dat van zijn beste vriend Ken. Ze zijn beiden getraind door een man genaamd Gouken, die hem heeft geadopteerd en opgevoed. Ryu reist de wereld rond, en wordt vaak door anderen beschreven als saai. Hoewel hij afstandelijk en emotieloos lijkt te zijn, is Ryu eigenlijk een aardig en goedhartig persoon. Hij respecteert anderen met vaardigheden die gelijk zijn aan de zijne. Ryu's doel is om een totale meester te worden in zijn vechtstijl.
+                        
+                Ryu is een van de deelnemers aan het World Warrior tournament. Na het verslaan van alle uitdagers verdient Ryu het recht om zijn gastheer, Sagat te bestrijden. Ryu verslaat Sagat met een bijna fatale Shoryuken, dat een groot litteken achterlaat op de borst van Sagat. Nadat hij weer bij bewustzijn komt en ontdekt wat er gebeurd is, zweert Sagat wraak op Ryu. Ryu laat Sagat achter en keert terug naar zijn huis, en ziet dat Gouken, zijn meester en adoptievader, blijkbaar is vermoord. Nadat hij erachter komt dat Akuma zijn adoptievader heeft bevochten en vermoedelijk heeft vermoord, begint Ryu om de wereld te reizen om deze man te vinden.</p>
+                </div>
+
+                
+
+               
+
+
+
+    </header>
+
+
+</body>
+</html>
